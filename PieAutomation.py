50ec29d6a3b2b7b9e66e1395040ab8aa770f5f1c
JohnDoe <contact@jlrroofing.com>
2025-08-07 15:49:40 -0700

    Working on Static Site

--- PieAutomation.py
+++ PieAutomation.py
@@ -1,37 +1,38 @@
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ResourceManagementClient
-from azure.mgmt.resource.resources.models import ResourceGroup
 from azure.mgmt.storage import StorageManagementClient
-
 import os
 
 primary_subscription = os.environ.get("SUBSCRIPTION_ID")
+
 resource_client = ResourceManagementClient(
     credential=DefaultAzureCredential(), 
     subscription_id=primary_subscription
     )
-storge_client = StorageManagementCleint(
-    crednetial=DefaultAzureCredential(),
+storage_client = StorageManagementClient(
+    credential=DefaultAzureCredential(),
     subscription_id=primary_subscription
 )
 
-rg = "pietestgonewonglol1233"
-rg_location = "westus"
-rg_setup = ResourceGroup(location=rg_location)
+rg = "rg-pietestgonewonglol"
+sa = "sapietestgonewonglo1"
 
-def new_storageaccount():
+
+def new_resourcegroup():
     try:
         print(f"Attempting to create Resource Group {rg}")
         resource_client.resource_groups.create_or_update(
-        resource_group_name=rg,
-        parameters=rg_setup
+            resource_group_name=rg,
+            parameters={
+            "location": "west us"
+            }
         )
         print(f"{rg} has been created!")
 
     except Exception as e:
         print("We ran into an issue creating to Resource Group, please read the errro below:")
 
-def remove_storageaccount():
+def remove_resourcegroup():
     try:
         print(f"Deleintg Resource Group {rg}")
         resource_client.resource_groups.begin_delete(
@@ -43,13 +44,47 @@ def remove_storageaccount():
         print("We ran into an issue")
         print({e})
 
-match = "pie"
+def new_storageaccount():
+    try:
+        print(f"Creating Storage Account {sa}")
+        storage_client.storage_accounts.begin_create(
+            resource_group_name=rg,
+            account_name=sa,
+            parameters= {
+                "sku":{
+                    "name": "Standard_LRS"
+                },
+                "kind": "StorageV2",
+                "location": "west us",
+                "access_tier": "Hot"
+            }
+        )
 
-for each in resource_client.resource_groups.list():
-    if match.lower() in each.name.lower():
-        print(each.name.lower())
-        resource_client.resource_groups.begin_delete(resource_group_name=each.name)
+    except Exception as e:
+        print("Something went wrong")
+        print({e})
+
+def new_staticsite():
+    try:
+        print("Creating static blob site")
+        storage_client.blob_services.set_service_properties(
+            resource_group_name=rg,
+            account_name=sa,
+            parameters= {
+                "static_website": {
+                    "enabled": True,
+                    "index_document": "index.html",
+                    "error_document_404_path": "404.html"
+                }
+            }
+        )
+
+    except Exception as e:
+        print("Something went wrong")
+        print({e})
 
 
+#new_resourcegroup()
 #new_storageaccount()
-#remove_storageaccount()
+new_staticsite()
+#remove_resourcegroup()
