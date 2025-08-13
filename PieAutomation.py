d7657803e295e82d36af9b60ba3ea9a97a314f27
JohnDoe <contact@jlrroofing.com>
2025-08-13 10:50:59 -0700

    Automated Static Site

--- PieAutomation.py
+++ PieAutomation.py
@@ -5,7 +5,6 @@ from azure.storage.blob import StaticWebsite, BlobServiceClient
 import os
 
 primary_subscription = os.environ.get("SUBSCRIPTION_ID")
-bob_connect_string = os.environ.get("AZURE_STORAGE_CONNECTION_STRING")
 
 resource_client = ResourceManagementClient(
         credential=DefaultAzureCredential(), 
@@ -15,12 +14,9 @@ storage_client = StorageManagementClient(
         credential=DefaultAzureCredential(),
         subscription_id=primary_subscription
     )
-blob_client = BlobServiceClient.from_connection_string(
-        bob_connect_string
-        )
 
-rg = "rg-pietestgonewonglol"
-sa = "sapietestgonewonglo1"
+rg = "rg-wearereallyprogsing1"
+sa = "sawearereallyprogsing1"
 
 
 def new_resourcegroup():
@@ -33,19 +29,8 @@ def new_resourcegroup():
             }
         )
         print(f"{rg} has been created!")
+        new_storageaccount()
 
-    except Exception as e:
-        print("We ran into an issue")
-        print({e})
-
-def remove_resourcegroup():
-    try:
-        print(f"Deleintg Resource Group {rg}")
-        resource_client.resource_groups.begin_delete(
-            resource_group_name=rg
-        )
-        print(f"It has been removed from this universe")
-    
     except Exception as e:
         print("We ran into an issue")
         print({e})
@@ -53,7 +38,7 @@ def remove_resourcegroup():
 def new_storageaccount():
     try:
         print(f"Creating Storage Account {sa}")
-        storage_client.storage_accounts.begin_create(
+        poller = storage_client.storage_accounts.begin_create(
             resource_group_name=rg,
             account_name=sa,
             parameters= {
@@ -65,6 +50,9 @@ def new_storageaccount():
                 "access_tier": "Hot"
             }
         )
+        poller.result()
+        print("Storage account has been created")
+        new_staticsite()
 
     except Exception as e:
         print("Something went wrong")
@@ -72,20 +60,39 @@ def new_storageaccount():
 
 def new_staticsite():
     try:
+        key_object = storage_client.storage_accounts.list_keys(rg, sa)
+        key_1 = key_object.keys[0].value
+        connection_key = f"DefaultEndpointsProtocol=https;AccountName={sa};AccountKey={key_1};EndpointSuffix=core.windows.net"
+        blob_client = BlobServiceClient.from_connection_string(
+            connection_key)
+
         print("Creating static blob site")
         static_site_settings = StaticWebsite(
             enabled=True,
             index_document="Index.html",
             error_document404_path="404.html"
         )
-        blob_client.set_service_properties(static_website=static_site_settings)
+        blob_client.set_service_properties(
+            static_website=static_site_settings
+            )
+        print("Static site settings have been setup")
 
     except Exception as e:
         print("Something went wrong")
         print({e})
 
+def remove_resourcegroup():
+    try:
+        print(f"Deleintg Resource Group {rg}")
+        poller = resource_client.resource_groups.begin_delete(
+            resource_group_name=rg
+        )
+        poller.result()
+        print(f"The resource group {rg} has been deleted")
+    
+    except Exception as e:
+        print("We ran into an issue")
+        print({e})
 
-#new_resourcegroup()
-#new_storageaccount()
-new_staticsite()
+new_resourcegroup()
 #remove_resourcegroup()
