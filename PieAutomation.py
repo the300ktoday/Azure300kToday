17c520dde4ea32aeb28715f8ad4856a006807847
JohnDoe <contact@jlrroofing.com>
2025-08-21 17:23:56 -0700

    Working static site, progress on FrontDoor

--- PieAutomation.py
+++ PieAutomation.py
@@ -1,10 +1,29 @@
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.storage import StorageManagementClient
-from azure.storage.blob import StaticWebsite, BlobServiceClient
+from azure.storage.blob import StaticWebsite, BlobServiceClient, ContentSettings
+from azure.mgmt.frontdoor import FrontDoorManagementClient
+from azure.mgmt.frontdoor.models import (
+    FrontDoor,
+    RoutingRule,
+    BackendPool,
+    FrontendEndpoint,
+    HealthProbeSettings,
+    LoadBalancingSettings
+)
 import os
 
-primary_subscription = os.environ.get("SUBSCRIPTION_ID")
+primary_subscription = os.environ.get("AZURE_SUBSCRIPTION_ID")
+rg = "rg-timetokeepgoing101"
+sa = "satimetokeepgoing101"
+raw_file_404 = r"C:\Github\Azure300kToday\404.html"
+raw_file_index = r"C:\Github\Azure300kToday\Index.html"
+file_404 = os.path.basename(raw_file_404)
+file_index = os.path.basename(raw_file_index)
+fdname = "cdnisreallyworkingkinodertoten"
+backend_host = "myresumeisonline0329.azurewebsites.net"
+frontend_hostname = "myresumeisonline0329.azurefd.net"
+
 
 resource_client = ResourceManagementClient(
         credential=DefaultAzureCredential(), 
@@ -14,11 +33,11 @@ storage_client = StorageManagementClient(
         credential=DefaultAzureCredential(),
         subscription_id=primary_subscription
     )
-
-rg = "rg-wearereallyprogsing1"
-sa = "sawearereallyprogsing1"
-
-
+frontdoor_client = FrontDoorManagementClient(
+        credential=DefaultAzureCredential(),
+        subscription_id=primary_subscription
+    )
+    
 def new_resourcegroup():
     try:
         print(f"Attempting to create Resource Group {rg}")
@@ -33,7 +52,7 @@ def new_resourcegroup():
 
     except Exception as e:
         print("We ran into an issue")
-        print({e})
+        print(e)
 
 def new_storageaccount():
     try:
@@ -56,7 +75,7 @@ def new_storageaccount():
 
     except Exception as e:
         print("Something went wrong")
-        print({e})
+        print(e)
 
 def new_staticsite():
     try:
@@ -76,10 +95,67 @@ def new_staticsite():
             static_website=static_site_settings
             )
         print("Static site settings have been setup")
+        new_sitefiles(blob_client)
 
     except Exception as e:
         print("Something went wrong")
-        print({e})
+        print(e)
+
+def new_sitefiles(service_client):
+    try:
+        print(f"Attempting to add files to Container")
+        container_name = "$web"
+        container_client = service_client.get_container_client(container_name)
+        blob_client = container_client.get_blob_client(file_index)
+        file_type = ContentSettings(
+            content_type="text/html"
+        )
+        with open(file_index, "rb") as data:
+            blob_client.upload_blob(data, overwrite=True, content_settings=file_type)
+
+        blob_client = container_client.get_blob_client(file_404)
+        with open (file_404, "rb") as data:
+            blob_client.upload_blob(data, overwrite=True, content_settings=file_type)
+        new_frontdoor()
+    
+    except Exception as e:
+        print("Something went wrong")
+        print(e)
+
+def new_frontdoor():
+    backend_pool_name = "Thisisthebackend"
+    backend_pool_settings = BackendPool(
+        name=backend_pool_name,
+        backends=[{
+            "address": backend_host,
+            "http_port": 80,
+            "https_port": 443,
+            "enabled_sate": "Enabled"
+        }],
+        health_probe_settings={
+            "path": "/",
+            "protocol": "Htpp",
+            "interval_in_seconds": 30
+        },
+        load_balancing_settings={
+            "sample_size": 4,
+            "successful_samples_required": 2
+        }
+    )
+
+
+    front_door_parameters = FrontDoor(
+        location="westus",
+
+
+    )
+
+    frontdoor_client.front_doors.begin_create_or_update(
+        resource_group_name=rg,
+        front_door_name=fdname,
+        front_door_parameters=
+    )
+
 
 def remove_resourcegroup():
     try:
@@ -92,7 +168,7 @@ def remove_resourcegroup():
     
     except Exception as e:
         print("We ran into an issue")
-        print({e})
+        print(e)
 
-new_resourcegroup()
-#remove_resourcegroup()
+#new_resourcegroup()
+remove_resourcegroup()
